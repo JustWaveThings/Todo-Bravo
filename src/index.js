b5e6b7d4709cbd211e5a70cb7b1de34dc69397ae
JustWaveThings <justwavethings@proton.me>
2023-02-19 17:46:23 -0500

    Create / test spaLayout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+/* eslint-disable no-console */
 import './style.css';
+import container from './spaLayout';
+
+console.log(container);
---
+++ src/spaLayout.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import './style.css';
+import createElement from './createElement';
+
+const container = document.getElementsByClassName('container');
+
+export default container;
