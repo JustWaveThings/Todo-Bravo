1ac86eb03c4627c657cdd58abda817e0fad4a898
JustWaveThings <justwavethings@proton.me>
2023-02-19 17:45:34 -0500

    Remove cli dashboard - takes up too much real estate.

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,26 @@ module.exports = {
 			{
 				test: /\.js$/,
 				enforce: 'pre',
-				use: ['source-map-loader'],
+				use: [
+					{
+						loader: 'source-map-loader',
+						options: {
+							filterSourceMappingUrl: (url, resourcePath) => {
+								if (/broker-source-map-url\.js$/i.test(url)) {
+									return false;
+								}
+
+								if (
+									/keep-source-mapping-url\.js$/i.test(resourcePath)
+								) {
+									return 'skip';
+								}
+
+								return true;
+							},
+						},
+					},
+				],
 			},
 		],
 	},
