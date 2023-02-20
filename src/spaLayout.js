ef069b4a1a00b7583fb6fd2d826603f8f9fbcbdb
JustWaveThings <justwavethings@proton.me>
2023-02-20 17:55:58 -0500

    Add semantic structure I want to create with DOM

--- src/spaLayout.js
+++ src/spaLayout.js
@@ -3,6 +3,27 @@
 /* eslint-disable no-unused-vars */
 import './style.css';
 
+// page layout structure
+{
+	/* <body>
+	<div id="pageContainer">
+		<header id="pageHeader"></header>
+		<div id="contentContainer" class="clearfix">
+			<nav id="pageNav"></nav>
+
+			<section id="pageSection">
+				<header class="sectionHeader"></header>
+				<article class="sectionArticle"></article>
+				<footer class="sectionFooter"></footer>
+			</section>
+
+			<aside id="pageAside"></aside>
+		</div>
+		<footer id="pageFooter"></footer>
+	</div>
+</body>; */
+}
+
 const container = document.querySelector('.container');
 
 export default container;
