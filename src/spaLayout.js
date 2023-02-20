e524426bb60d7391e09647666ad80d4e2af28038
JustWaveThings <justwavethings@proton.me>
2023-02-20 17:48:21 -0500

    changed to querySelector

--- src/spaLayout.js
+++ src/spaLayout.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import './style.css';
-import createElement from './createElement';
 
-const container = document.getElementsByClassName('container');
+const container = document.querySelector('.container');
 
 export default container;
