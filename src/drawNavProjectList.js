b40e8c2ba80650f0850588415e8285efa0ed54dc
JustWaveThings <justwavethings@proton.me>
2023-02-22 12:28:36 -0500

    WIP -render project list to nav ui

--- src/drawNavProjectList.js
+++ src/drawNavProjectList.js
@@ -2,11 +2,13 @@ import { projectList } from './newProjectDialog';
 import { navProjectList } from './pl-nav';
 
 document.addEventListener('DOMContentLoaded', function () {
-	const listParent = document.querySelector('.nav-project-list');
-	console.log(listParent);
+	const listParent = document.querySelectorAll(
+		'.nav-project-list'
+	)[0];
+	return listParent;
 });
 
-export function updateNavProjectList() {
+export function updateNavProjectList(listParent, projectList) {
 	projectList.forEach((project) => {
 		const listItem = document.createElement('li');
 		listItem.className = 'nav-project-item';
