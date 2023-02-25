ea754ac4ec879b174ad0796e15ca1aae2c8d25bb
JustWaveThings <justwavethings@proton.me>
2023-02-25 14:51:07 -0500

    removeProjectFromLocalStorage() removes project and tasks

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -1,29 +1,9 @@
-/* 
-
-Psuedo Code -  Local Storage
-
-1. Create a function that will update the local storage with the current state of each of the 3 arrays (projectList, taskList, and todoList)  using JSON.stringify(). Good news is that all the data is already in the correct format.
-
-  Are we looking at a find and replace for local storage, or an entire rewrite of the local storage?
-
-2. Create a function that will retrieve the data from local storage and update the 3 arrays (projectList, taskList, and todoList) with the data from local storage using JSON.parse().
-
-3. Create a function that will clear the local storage.
-
-4 Create a function that will check if there is data in local storage. If there is data in local storage, then call the function that will retrieve the data from local storage and update the 3 arrays (projectList, taskList, and todoList) with the data from local storage using JSON.parse(). If there is no data in local storage, then do nothing. This needs to be called when the page loads.
-
-5. Call the function that will check if there is data in local storage.
-
-6. Deterimine when to call the function that will update the local storage with the current state of each of the 3 arrays (projectList, taskList, and todoList)  using JSON.stringify().
-
-*/
-
 import projectList from '../dataStores/projectList';
 import todoObjects from '../dataStores/todoObjects';
 
 export function updateLocalStorage() {
 	localStorage.setItem('projectList', JSON.stringify(projectList));
-	localStorage.setItem('taskList', JSON.stringify(todoObjects));
+	localStorage.setItem('todoObjects', JSON.stringify(todoObjects));
 }
 
 export function getDataFromLocalStorage() {
@@ -57,3 +37,39 @@ export function checkLocalStorage() {
 		console.log('localStorage is empty');
 	}
 }
+
+export function removeTaskFromLocalStorage(id) {
+	const items = JSON.parse(localStorage.getItem('todoObjects')) || [];
+	console.log(items);
+	const index = items.findIndex((item) => item.id === id);
+	console.log(index);
+	items.splice(index, 1);
+	localStorage.setItem('todoObjects', JSON.stringify(items));
+}
+
+export function removeProjectFromLocalStorage(project) {
+	// remove from projectList
+	const items = JSON.parse(localStorage.getItem('projectList')) || [];
+	console.log(items);
+	const index = items.findIndex((item) => item === project);
+	console.log(index);
+	items.splice(index, 1);
+	localStorage.setItem('projectList', JSON.stringify(items));
+
+	// Remove the objects that have that project name from todoObjects array.
+	const todoItems =
+		JSON.parse(localStorage.getItem('todoObjects')) || [];
+	console.log(todoItems);
+
+	let todoIndex = 0;
+	while (todoIndex !== -1) {
+		todoIndex = todoItems.findIndex(
+			(item) => item.project === project
+		);
+		todoItems.splice(todoIndex, 1);
+	}
+	console.log(todoIndex);
+	localStorage.setItem('todoObjects', JSON.stringify(todoItems));
+}
+
+removeProjectFromLocalStorage('test');
